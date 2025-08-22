bf66a46bd694ed6b827951f782c1e98eecef0957
BiMq0 <bryangod194@gmail.com>
2025-08-22 12:43:39 -0400

    se agrego funcionalidad de eliminacion y edicion de libros, ademas de un renderizado condicional en caso de no contar con ningun libro en la lista estatica

--- AppBlazor.Client/Servicios/RepresentantesServicio.cs
+++ AppBlazor.Client/Servicios/RepresentantesServicio.cs
@@ -7,7 +7,29 @@ namespace AppBlazor.Client.Servicios
 {
     public static class RepresentantesServicio
     {
-        public static List<RepresentanteFormCLS> lstRepresentantes = new();
+            public static List<RepresentanteFormCLS> lstRepresentantes = new() 
+            {
+                new RepresentanteFormCLS
+    {
+        Num_Empl = 1,
+        Nombre = "Juan Pérez",
+        Edad = 30,
+        Cargo = "Vendedor",
+        FechaContrato = new DateTime(2015, 5, 10),
+        Cuota = 50,
+        Ventas = 60
+    },
+    new RepresentanteFormCLS
+    {
+        Num_Empl = 2,
+        Nombre = "María López",
+        Edad = 28,
+        Cargo = "Vendedor",
+        FechaContrato = new DateTime(2018, 3, 22),
+        Cuota = 40,
+        Ventas = 45
+    },
+            };
 
         public static void AgregarLibro(RepresentanteFormCLS nuevoRepresentante)
         {
